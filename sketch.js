0fd6119b6635ba976486fbea1cfed1dedf4f2d62
navapro <45469562+navapro@users.noreply.github.com>
2019-11-16 17:21:36 -0600

    added mouse controls to the objects

--- sketch.js
+++ sketch.js
@@ -8,18 +8,28 @@
 //  -used class.
 //  -make the zombie kill the hero (AI).
 
+// set all of this to matter so i dont have to say " Matter. " every where.
+//const { Engine, World, Bodies, Mouse, MouseConstraint, Constraint } = Matter;
 let ground;
 let bird;
 let box;
 let engine,world;
+let mouseControl;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  const CANVAS = createCanvas(windowWidth, windowHeight);
   engine = Matter.Engine.create();
   world = engine.world;
   ground = new Ground(width/2,height-10,width,20);
   box = new Box(width/2, height/2,100,175);
   bird = new Bird(width/6, height-200,50);
+  
+  let mouse = Matter.Mouse.create(canvas.elt);
+  const OPTIONS = {
+    mouse: mouse
+  }
+  mouseControl = Matter.MouseConstraint.create(engine,OPTIONS);
+  Matter.World.add(world,mouseControl);
 }
 
 function draw() {
