a303d51b785be51b1c435905f003230099d1f274
navapro <45469562+navapro@users.noreply.github.com>
2019-11-18 18:30:42 -0600

    tried to fix collition and added images for charecter

--- box.js
+++ box.js
@@ -1,8 +1,11 @@
 class Box {
   
     constructor(x, y, w, h) {
+      const options = {
+        restitution: 0.5
+      }  
       
-      this.body = Matter.Bodies.rectangle(x, y, w, h);
+    this.body = Matter.Bodies.rectangle(x, y, w, h, options);
       Matter.World.add(world, this.body);
       this.w = w;
       this.h = h;
