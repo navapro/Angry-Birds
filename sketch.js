627eec8fb47d7b8660786c2b1c271ec36fb47d5a
navapro <45469562+navapro@users.noreply.github.com>
2019-11-19 13:48:44 -0600

    added images to the  sketch

--- sketch.js
+++ sketch.js
@@ -1,55 +1,67 @@
-// State variable assignment.
-// Navaneeth Krishna.
-// 22- oct - 2019
-// used the state variable for menu page and starting the game.
-// Extra for Experts:
-//  -used rotate and translate.
-//  -used html to add gif image as background.
-//  -used class.
-//  -make the zombie kill the hero (AI).
+const { Engine, World, Bodies, Mouse, MouseConstraint, Constraint } = Matter;
 
-// set all of this to matter so i dont have to say " Matter. " every where.
-//const { Engine, World, Bodies, Mouse, MouseConstraint, Constraint } = Matter;
 let ground;
+const boxes = [];
 let bird;
-let box;
-let engine,world;
+let world, engine;
 let mConstraint;
-let hit = false;
+let slingshot;
 
-let birdX = 0;
-let birdY = 0;
-let birdR = 50;
+let dotImg;
+let boxImg;
+let bkgImg;
+
+function preload() {
+  dotImg = loadImage('images/dot.png');
+  boxImg = loadImage('images/equals.png');
+  bkgImg = loadImage('images/skyBackground.png');
+}
 
 function setup() {
-  const CANVAS = createCanvas(windowWidth, windowHeight);
-  engine = Matter.Engine.create();
+  const canvas = createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
   world = engine.world;
-  ground = new Ground(width/2,height -10,width,20);
-  box = new Box(width/2, height/2,100,175);
-  bird = new Bird(width/6, height-200,50);
+  ground = new Ground(width / 2, height - 10, width, 20);
+  for (let i = 0; i < 3; i++) {
+    boxes[i] = new Box(width/1.5, (height-200) - i * 75, 126, 150);
+  }
+  bird = new Bird(300, height/1.2, 40);
 
+  slingshot = new SlingShot(300, height/1.2, bird.body);
+
+  const mouse = Mouse.create(canvas.elt);
+  const options = {
+    mouse: mouse,
+  }
+
+  // A fix for HiDPI displays
+  mouse.pixelRatio = pixelDensity();
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    World.remove(world, bird.body);
+    bird = new Bird(300, height/1.2, 40);
+    slingshot.attach(bird.body);
+  }
+
+}
+
+function mouseReleased() {
+  setTimeout(() => {
+    slingshot.fly();
+  }, 100);
 }
 
 function draw() {
- 
-  background(0);
-  
+  background(bkgImg);
   Matter.Engine.update(engine);
   ground.show();
-  box.show();
+  for (let box of boxes) {
+    box.show();
+  }
+  slingshot.show();
   bird.show();
-  checkCollide();
-
-  
 }
-function checkCollide(){
-  
-  if (mouseIsPressed){
-  hit = collidePointCircle(mouseX,mouseY,birdX,birdY,birdR*2)
-  }
-  else {
-    hit = false;
-  }
-}
-
