d8781f11aecaadf1cc0b76f7ae871548a6c914d5
navapro <45469562+navapro@users.noreply.github.com>
2019-11-23 11:19:48 -0600

    moved all the preload to a different file

---
+++ preload.js
@@ -0,0 +1,10 @@
+function preload() {
+  birdImg = loadImage('assets/redBird.png');
+  boxImg = loadImage('assets/wood.png');
+  bkgImg = loadImage('images/skyBackground.png');
+  slingShotImgLeft = loadImage('assets/slingshotLeft.png');
+  slingShotImgRight = loadImage('assets/slingshotRight.png');
+  slingShotBandBack = loadImage('assets/slingshotBackSling.png');
+  slingShotBandFront = loadImage('assets/slingshotFrontSling.png');
+  minnionPigImg = loadImage('assets/pig.png');
+}
