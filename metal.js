8bb1c16a9901198131ae3c6b4f5e083e0993a342
navapro <45469562+navapro@users.noreply.github.com>
2019-12-11 19:08:04 -0600

    Update metal.js

---
+++ metal.js
@@ -0,0 +1,31 @@
+class Metal {
+  
+    constructor(x, y, w, h,img) {
+      const options = {
+        restitution:.1
+      }    
+      this.body = Matter.Bodies.rectangle(x, y, w, h, options);
+      Matter.Body.setMass(this.body, this.body.mass*2);
+      Matter.World.add(world, this.body);
+      this.w = w;
+      this.h = h;
+      this.img = img;
+    }
+    
+    show() {
+      World.remove(world, mConstraint);
+      const pos = this.body.position;
+      const angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      fill(255);
+      rectMode(CENTER);
+      imageMode(CENTER);
+      image(this.img, 0, 0, this.w, this.h);
+      pop(); 
+    }
+    delete(){
+      Matter.World.remove(world, this.body);
+    }
+  }
