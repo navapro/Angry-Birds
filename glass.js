06c29a1a305d964892f46cb250f27d7f8f93d02e
navapro <45469562+navapro@users.noreply.github.com>
2019-12-13 18:06:09 -0600

    added glass

---
+++ glass.js
@@ -0,0 +1,34 @@
+class Glass {
+  
+    constructor(x, y, w, h,img) {
+      const options = {
+        restitution:0
+      }    
+      this.body = Matter.Bodies.rectangle(x, y, w, h);
+      Matter.Body.setMass(this.body, this.body.mass*5);
+      this.body.density=  1;
+      this.body.friction =  .5;
+      this.body.frictionAir =  .015;
+      Matter.World.add(world, this.body,options);
+      this.w = w;
+      this.h = h;
+      this.img = img;
+    }
+    
+    show() {
+      World.remove(world, mConstraint);
+      const pos = this.body.position;
+      const angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      fill(255);
+      rectMode(CENTER);
+      imageMode(CENTER);
+      image(this.img, 0, 0, this.w, this.h);
+      pop(); 
+    }
+    delete(){
+      Matter.World.remove(world, this.body);
+    }
+  }
